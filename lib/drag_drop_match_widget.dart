0fbb1b35a90a0f8f85e7832f51c3aeb8328658d3
sabin khanal <sabin.khanal.33@gmail.com>
2022-02-07 14:23:12 +0545

    First release

--- lib/drag_drop_match_widget.dart
+++ lib/drag_drop_match_widget.dart
@@ -2,17 +2,65 @@ library drag_drop_match_widget;
 
 import 'package:flutter/material.dart';
 
-import 'drag_drop_model.dart';
+class DragDropItem {
+  final String key;
+  final String value;
+  Widget? dragChild;
+  Widget? dropChild;
+  IconData? iconData;
+  Widget? feedbackItem;
+  Widget? childWhenDragging;
+  bool willAccept = true;
+  bool isAccepted;
+  DragDropItem(
+      {required this.key,
+      required this.value,
+      this.iconData,
+      this.dragChild,
+      this.dropChild,
+      this.feedbackItem,
+      this.childWhenDragging,
+      this.isAccepted = false}) {
+    feedbackItem =
+        this.dragChild ?? Icon(iconData, size: 30, color: Colors.teal);
+    dragChild = this.dragChild ??
+        Text(key,
+            style: TextStyle(
+              fontSize: 20,
+            ));
+    dropChild = dropChild ??
+        Text(value,
+            style: TextStyle(
+              fontSize: 20,
+            ));
+    childWhenDragging = dragChild ??
+        Text(value,
+            style: TextStyle(
+              fontSize: 20,
+            ));
+  }
 
-typedef void DragDropAction(DragDropItem item);
+  factory DragDropItem.fromMap(MapEntry map) {
+    return DragDropItem(
+        key: map.key,
+        value: map.value,
+        dragChild: Text(map.key),
+        dropChild: Text(map.value));
+  }
+}
+
+typedef DragDropAction = void Function(DragDropItem item);
 
 ///Drag drop widget for creating match views
 ///Input your stuffs on [items]
 ///[onMatched] is handler for matched event
 ///[onMisMatched] is handler for not matched event
 class DragDropWidget extends StatefulWidget {
+  //list of items you want to embed in drag drop UI
   final List<DragDropItem> items;
+  //on matched event
   final DragDropAction onMatched;
+  //on mismatched event
   final DragDropAction onMisMatched;
   DragDropWidget(
       {Key? key,
@@ -26,14 +74,17 @@ class DragDropWidget extends StatefulWidget {
 }
 
 class _DragDropWidgetState extends State<DragDropWidget> {
+  //The backdrop style : after drag drop is accepted
   final acceptedBackdropStyle =
       TextStyle(fontSize: 40, color: Colors.greenAccent);
+
+  //list for another side for matching
   List<DragDropItem> drop_lists = [];
   @override
   void initState() {
     super.initState();
 
-    //create drop targets
+    //create drop targets by shuffling the original list
     drop_lists = List.from(widget.items);
     drop_lists.shuffle();
   }
